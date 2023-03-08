f6d4b519802f63e2dd548be1fa33e2ed329d0cbf
albert <visionboy@nate.com>
2023-03-08 10:48:29 +0900

    Initial commit

---
+++ src/main/java/com/albert/autoDep/AutoDepApplication.java
@@ -0,0 +1,13 @@
+package com.albert.autoDep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albert/autoDep/AutoDepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albert.autoDep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
